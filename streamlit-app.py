ab2108e45f0be8b8a678ef8dc020a7a54acb3cee
Harsh Roniyar <112925148+hsr-22@users.noreply.github.com>
2023-12-11 12:28:06 +0530

    Updated streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,4 +1,24 @@
 import streamlit as st
+import base64
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('https://github.com/hsr-22/MaxNumFinder-Streamlit/blob/main/app-bkg.png')
 
 def find_largest(num1, num2, num3):
     return max(num1, num2, num3)
